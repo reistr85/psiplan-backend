f737d55747d04156594d2c1b0b4811e3f88ca39a
Renan Reis <renan.reis@esig.com.br>
2021-01-05 00:04:32 -0300

    Refatorando algumas rotas.

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ Route::group(['prefix' => 'psiplan/v1', 'namespace' => 'API\\v1', 'middleware' =
     Route::post('forgot-password', 'UserController@forgotPassword');
 
     /*
-     * ListPsychologist 
+     * ListPsychologist
      * */
     Route::get('list-psychologist', 'ListPsychologistController@index');
     Route::get('get-filters', 'ListPsychologistController@getFilters');
@@ -103,4 +103,8 @@ Route::group(['prefix' => 'psiplan/v1', 'namespace' => 'API\\v1', 'middleware' =
     Route::delete('availability/{id}', 'PsychologistAvailabilityCalendarController@destroy');
     Route::post('get-service-hours', 'PsychologistAvailabilityCalendarController@getServiceHours');
     Route::post('delete-all-hours', 'PsychologistAvailabilityCalendarController@destroyAll');
+
+    /*
+     * Payment Client
+     * */
 });
