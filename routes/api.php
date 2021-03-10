f42a5ec9f3922eaf28e91824bd18bc439d5b6ea2
Renan Reis <reistr85@gmail.com>
2021-03-09 23:42:53 -0300

    Iniciando com a recuperação de senha.

--- routes/api.php
+++ routes/api.php
@@ -2,188 +2,193 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'psiplan/v1', 'namespace' => 'API\\v1', 'middleware' => ['apiKey']], function() {
+Route::group(['prefix' => 'psiplan/v1', 'middleware' => ['apiKey']], function() {
+    Route::post('forgot_password', 'Auth\ForgotPasswordController@password');
 
-    /*
-     * AuthController
-     * */
-    Route::post('login', 'AuthController@login');
-    Route::post('forgot-password', 'AuthController@forgotPassword');
+    Route::group(['namespace' => 'API\\v1'], function() {
 
-    /*
-     * UserController
-     * */
-    Route::resource('users', 'UserController');
-    Route::post('forgot-password', 'UserController@forgotPassword');
-
-    /*
-     * ListPsychologist
-     * */
-    Route::get('list-psychologist', 'ListPsychologistController@index');
-    Route::get('get-filters', 'ListPsychologistController@getFilters');
-    Route::post('get-psychologist/{id}', 'ListPsychologistController@show');
-    Route::get('get-psychologist-availability/{psychologist_id}/{type_service_id}', 'ListPsychologistController@getPsychologistAvailabilityByTypeServiceId');
-    Route::post('get-psychologist-service-hours', 'ListPsychologistController@getServiceHours');
-
-    /*
-     * CityController
-     * */
-    Route::post('cities-by-name', 'CityController@getCitiesByName');
-
-    /*
-     * PagarmePlansController
-     * */
-    Route::resource('pagarme/plans', 'PagarmePlansController');
-
-    /*
-     * SMS
-     * */
-//    Route::post('sms', 'AuthController@sms');
-});
-
-Route::group(['prefix' => 'psiplan/v1', 'namespace' => 'API\\v1', 'middleware' => ['apiKey', 'apiJwt']], function() {
-
-    Route::get('/', function(){ return response()->json(['status' => true]); });
-
-    /*
-     * AuthController
-     * */
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
-    Route::post('logout', 'AuthController@logout');
-
-    /*
-     * ProfileController
-     * */
-    Route::get('account/profile', 'ProfileController@index');
-    Route::post('account/profile', 'ProfileController@show');
-    Route::put('account/profile/', 'ProfileController@update');
-    Route::post('account/profile/avatar', 'ProfileController@update');
-    Route::post('account/profile/gallery_tow', 'ProfileController@update');
-    Route::post('account/profile/gallery_three', 'ProfileController@update');
-    Route::post('account/profile/gallery_four', 'ProfileController@update');
-    Route::post('account/profile/gallery_five', 'ProfileController@update');
-    //Route::post('account/delete-image-gallery', 'ProfileController@deleteImageGallery');
+        /*
+         * AuthController
+         * */
+        Route::post('login', 'AuthController@login');
+        Route::post('forgot-password', 'AuthController@forgotPassword');
 
 
-    /*
-     * PsychologistAcademicFormationController
-     * */
-    Route::post('account/formations', 'PsychologistAcademicFormationController@store');
-    Route::delete('account/formations/{id}', 'PsychologistAcademicFormationController@destroy');
+        /*
+         * UserController
+         * */
+        Route::resource('users', 'UserController');
+        Route::post('forgot-password', 'UserController@forgotPassword');
 
-    /*
-     * PsychologistSpecialtyController
-     * */
-    Route::post('account/specialties', 'PsychologistSpecialtyController@store');
+        /*
+         * ListPsychologist
+         * */
+        Route::get('list-psychologist', 'ListPsychologistController@index');
+        Route::get('get-filters', 'ListPsychologistController@getFilters');
+        Route::post('get-psychologist/{id}', 'ListPsychologistController@show');
+        Route::get('get-psychologist-availability/{psychologist_id}/{type_service_id}', 'ListPsychologistController@getPsychologistAvailabilityByTypeServiceId');
+        Route::post('get-psychologist-service-hours', 'ListPsychologistController@getServiceHours');
 
-    /*
-     * PsychologistDocumentController
-     * */
-    Route::post('account/documents', 'PsychologistDocumentController@store');
+        /*
+         * CityController
+         * */
+        Route::post('cities-by-name', 'CityController@getCitiesByName');
 
-    /*
-     * AccountController
-     * */
-    Route::get('account/account', 'AccountController@index');
-    Route::post('account/account', 'AccountController@store');
+        /*
+         * PagarmePlansController
+         * */
+        Route::resource('pagarme/plans', 'PagarmePlansController');
 
-    /*
-     * QueryController
-     * */
-    Route::get('account/queries', 'QueryController@index');
-    Route::post('account/queries/type-service', 'QueryController@storeTypeService');
-    Route::post('account/queries/info-extras', 'QueryController@storeInfoExtras');
-
-    /*
-     * PreferenceController
-     * */
-    Route::get('account/preferences', 'PreferenceController@index');
-    Route::put('account/preferences/{id}', 'PreferenceController@update');
-    Route::put('account/preferences/users/{id}', 'UserController@update');
-
-    /*
-     * CityController
-     * */
-    Route::post('cities', 'CityController@index');
-
-    /*
-     * PsychologistAvailabilityCalendarController
-     * */
-    Route::get('availability', 'PsychologistAvailabilityCalendarController@index');
-    Route::post('availability', 'PsychologistAvailabilityCalendarController@store');
-    Route::delete('availability/{id}', 'PsychologistAvailabilityCalendarController@destroy');
-    Route::post('get-service-hours', 'PsychologistAvailabilityCalendarController@getServiceHours');
-    Route::post('delete-all-hours', 'PsychologistAvailabilityCalendarController@destroyAll');
-    Route::post('delete-all-hours-day-selected', 'PsychologistAvailabilityCalendarController@destroyAllDaySelected');
-
-    /*
-     * PsychologistAvailabilityCalendarController
-     * */
-    Route::get('availability', 'PsychologistAvailabilityCalendarController@index');
-
-    /*
-     * TypeServicesController
-     * */
-    Route::post('psychologist/payment/plan', 'PaymentPlanPsychologistController@store');
-    Route::put('psychologist/payment/plan', 'PaymentPlanPsychologistController@update');
-
-    /*
-     *
-     * */
-    Route::resource('notifications', 'UserNotificationController');
-
-    Route::group(['prefix' => 'management'], function() {
-
-        Route::get('queries', 'ManagementQueryController@index');
-        Route::put('queries/{id}', 'ManagementQueryController@update');
-
-        Route::get('payments', 'ManagementPaymentController@index');
-
-        Route::get('receipts', 'ManagementReceiptController@index');
-
-        Route::get('evaluations', 'ManagementEvaluationController@index');
+        /*
+         * SMS
+         * */
+    //    Route::post('sms', 'AuthController@sms');
     });
 
+    Route::group(['prefix' => 'psiplan/v1', 'namespace' => 'API\\v1', 'middleware' => ['apiKey', 'apiJwt']], function() {
 
-    Route::group(['prefix' => 'client', 'middleware' => ['checkRouteClient']], function() {
-
-        /*
-        * Query
-        * */
-        Route::get('queries', 'ClientQueryController@index');
-        Route::get('queries/{id}', 'ClientQueryController@show');
-        Route::post('queries', 'ClientQueryController@store');
+        Route::get('/', function(){ return response()->json(['status' => true]); });
 
         /*
-        * Payment
-        * */
-        Route::get('payment', 'PaymentQueryClientController@index');
-        Route::post('payment', 'PaymentQueryClientController@store');
+         * AuthController
+         * */
+        Route::post('refresh', 'AuthController@refresh');
+        Route::post('me', 'AuthController@me');
+        Route::post('logout', 'AuthController@logout');
 
         /*
-        * Evaluation
-        * */
-        Route::post('evaluations', 'EvaluationController@store');
+         * ProfileController
+         * */
+        Route::get('account/profile', 'ProfileController@index');
+        Route::post('account/profile', 'ProfileController@show');
+        Route::put('account/profile/', 'ProfileController@update');
+        Route::post('account/profile/avatar', 'ProfileController@update');
+        Route::post('account/profile/gallery_tow', 'ProfileController@update');
+        Route::post('account/profile/gallery_three', 'ProfileController@update');
+        Route::post('account/profile/gallery_four', 'ProfileController@update');
+        Route::post('account/profile/gallery_five', 'ProfileController@update');
+        //Route::post('account/delete-image-gallery', 'ProfileController@deleteImageGallery');
+
 
         /*
-        * Client
-        * */
-        Route::get('clients/{id}', 'ClientController@show');
+         * PsychologistAcademicFormationController
+         * */
+        Route::post('account/formations', 'PsychologistAcademicFormationController@store');
+        Route::delete('account/formations/{id}', 'PsychologistAcademicFormationController@destroy');
 
         /*
-        * ClientProfile
-        * */
-        Route::put('profile', 'ClientProfileController@update');
+         * PsychologistSpecialtyController
+         * */
+        Route::post('account/specialties', 'PsychologistSpecialtyController@store');
 
         /*
-        * ClientBilling
-        * */
-        Route::put('billings', 'ClientBillingController@update');
+         * PsychologistDocumentController
+         * */
+        Route::post('account/documents', 'PsychologistDocumentController@store');
 
         /*
-        * ClientCoupons
-        * */
-        Route::resource('coupons', 'CouponController');
+         * AccountController
+         * */
+        Route::get('account/account', 'AccountController@index');
+        Route::post('account/account', 'AccountController@store');
+
+        /*
+         * QueryController
+         * */
+        Route::get('account/queries', 'QueryController@index');
+        Route::post('account/queries/type-service', 'QueryController@storeTypeService');
+        Route::post('account/queries/info-extras', 'QueryController@storeInfoExtras');
+
+        /*
+         * PreferenceController
+         * */
+        Route::get('account/preferences', 'PreferenceController@index');
+        Route::put('account/preferences/{id}', 'PreferenceController@update');
+        Route::put('account/preferences/users/{id}', 'UserController@update');
+
+        /*
+         * CityController
+         * */
+        Route::post('cities', 'CityController@index');
+
+        /*
+         * PsychologistAvailabilityCalendarController
+         * */
+        Route::get('availability', 'PsychologistAvailabilityCalendarController@index');
+        Route::post('availability', 'PsychologistAvailabilityCalendarController@store');
+        Route::delete('availability/{id}', 'PsychologistAvailabilityCalendarController@destroy');
+        Route::post('get-service-hours', 'PsychologistAvailabilityCalendarController@getServiceHours');
+        Route::post('delete-all-hours', 'PsychologistAvailabilityCalendarController@destroyAll');
+        Route::post('delete-all-hours-day-selected', 'PsychologistAvailabilityCalendarController@destroyAllDaySelected');
+
+        /*
+         * PsychologistAvailabilityCalendarController
+         * */
+        Route::get('availability', 'PsychologistAvailabilityCalendarController@index');
+
+        /*
+         * TypeServicesController
+         * */
+        Route::post('psychologist/payment/plan', 'PaymentPlanPsychologistController@store');
+        Route::put('psychologist/payment/plan', 'PaymentPlanPsychologistController@update');
+
+        /*
+         *
+         * */
+        Route::resource('notifications', 'UserNotificationController');
+
+        Route::group(['prefix' => 'management'], function() {
+
+            Route::get('queries', 'ManagementQueryController@index');
+            Route::put('queries/{id}', 'ManagementQueryController@update');
+
+            Route::get('payments', 'ManagementPaymentController@index');
+
+            Route::get('receipts', 'ManagementReceiptController@index');
+
+            Route::get('evaluations', 'ManagementEvaluationController@index');
+        });
+
+
+        Route::group(['prefix' => 'client', 'middleware' => ['checkRouteClient']], function() {
+
+            /*
+            * Query
+            * */
+            Route::get('queries', 'ClientQueryController@index');
+            Route::get('queries/{id}', 'ClientQueryController@show');
+            Route::post('queries', 'ClientQueryController@store');
+
+            /*
+            * Payment
+            * */
+            Route::get('payment', 'PaymentQueryClientController@index');
+            Route::post('payment', 'PaymentQueryClientController@store');
+
+            /*
+            * Evaluation
+            * */
+            Route::post('evaluations', 'EvaluationController@store');
+
+            /*
+            * Client
+            * */
+            Route::get('clients/{id}', 'ClientController@show');
+
+            /*
+            * ClientProfile
+            * */
+            Route::put('profile', 'ClientProfileController@update');
+
+            /*
+            * ClientBilling
+            * */
+            Route::put('billings', 'ClientBillingController@update');
+
+            /*
+            * ClientCoupons
+            * */
+            Route::resource('coupons', 'CouponController');
+        });
     });
 });
