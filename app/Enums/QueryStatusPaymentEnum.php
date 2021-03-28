7bb849d57ab5b9a5cdefa17adcceccc5f20a84fa
Renan Reis <reistr85@gmail.com>
2021-03-28 19:42:39 -0300

    Foi criado service para atualizar o status da consulta atraveis do post back

--- app/Enums/QueryStatusPaymentEnum.php
+++ app/Enums/QueryStatusPaymentEnum.php
@@ -10,4 +10,6 @@ class QueryStatusPaymentEnum extends Enum
 {
     public const STATUS_PAYMENT_PAID = 'paid';
     public const STATUS_PAYMENT_UNPAID = 'unpaid';
+    public const STATUS_PAYMENT_REFUSED = 'refused';
+    public const STATUS_PAYMENT_AUTHORIZED = 'authorized';
 }
