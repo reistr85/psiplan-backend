338d0fea9cc11358c1c74b846efaa05f31403a74
Renan Reis <reistr85@gmail.com>
2021-03-31 19:41:20 -0300

    Adicionar config group route dashboard

--- routes/api.php
+++ routes/api.php
@@ -43,9 +43,13 @@ Route::group(['prefix' => 'psiplan/v1'], function() {
 
     Route::group(['namespace' => 'API\\v1', 'middleware' => ['apiKey']], function() {
 
-        Route::group(['prefix' => 'dashboard', 'namespace' => 'Dashboard'], function() {
-            Route::resource('psychologists', 'PsychologistController');
-            Route::resource('clients', 'ClientsController');
+        Route::group(['prefix' => 'dashboard'], function() {
+            Route::post('login', 'AuthController@login');
+
+            Route::group(['namespace' => 'Dashboard', 'middleware' => ['apiJwt']], function() {
+                Route::resource('psychologists', 'PsychologistController');
+                Route::resource('clients', 'ClientsController');
+            });
         });
 
         /*
