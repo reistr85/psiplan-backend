baf1c5d41eff1a3c34a7a6bd66037db9cda57139
Renan Reis <reistr85@gmail.com>
2021-04-14 00:49:12 -0300

    Ativando e desavitanvo os psicologos

--- app/Http/Controllers/API/v1/Dashboard/PsychologistController.php
+++ app/Http/Controllers/API/v1/Dashboard/PsychologistController.php
@@ -8,19 +8,23 @@ use App\Services\API\v1\Dashboard\GetPsychologistService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Services\API\v1\Dashboard\UpdatePsychologistService;
 
 class PsychologistController extends Controller
 {
 
     private $get_all_psychologists_service;
     private $get_psychologist_service;
+    private $update_psychologist;
 
     public function __construct(
         GetAllPsychologistsService $get_all_psychologists_service,
-        GetPsychologistService $get_psychologist_service)
+        GetPsychologistService $get_psychologist_service,
+        UpdatePsychologistService $update_psychologist)
     {
         $this->get_all_psychologists_service = $get_all_psychologists_service;
         $this->get_psychologist_service = $get_psychologist_service;
+        $this->update_psychologist = $update_psychologist;
     }
 
     /**
@@ -61,7 +65,7 @@ class PsychologistController extends Controller
         try {
             $psychologist = $this->get_psychologist_service->execute($id);
 
-            return response()->json(['status' => false, 'message' => 'success', 'psychologist' => $psychologist], 200);
+            return response()->json(['status' => true, 'message' => 'success', 'psychologist' => $psychologist], 200);
         }catch(\Exception $ex){
             return response()->json(['status' => false, 'message' => $ex->getMessage()], 500);
         }
@@ -76,7 +80,13 @@ class PsychologistController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      try {
+          $this->update_psychologist->execute($id, $request->all());
+
+          return response()->json(['status' => true, 'message' => 'success'], 200);
+      }catch(\Exception $ex){
+          return response()->json(['status' => false, 'message' => $ex->getMessage()], 500);
+      }
     }
 
     /**
