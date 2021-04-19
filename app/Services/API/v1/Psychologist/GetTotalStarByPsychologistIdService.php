6df3e8c67fc12867dac87ab949713fbf656d4849
Renan Reis <reistr85@gmail.com>
2021-04-18 22:06:58 -0300

    Corrigindo Stars

--- app/Services/API/v1/Psychologist/GetTotalStarByPsychologistIdService.php
+++ app/Services/API/v1/Psychologist/GetTotalStarByPsychologistIdService.php
@@ -3,37 +3,26 @@
 
 namespace App\Services\API\v1\Psychologist;
 
+use App\Repositories\PsychologistRepository;
 use App\Repositories\QueryRepository;
 
 class GetTotalStarByPsychologistIdService
 {
     private $query_repository;
+    private $psychologist_repository;
 
     public function __construct(
-        QueryRepository $query_repository)
+        QueryRepository $query_repository,
+        PsychologistRepository $psychologist_repository)
     {
         $this->query_repository = $query_repository;
+        $this->psychologist_repository = $psychologist_repository;
     }
 
     public function execute(int $psychologist_id)
     {
-        $queries = $this->query_repository->getAllQueriesFindByPsychologistId($psychologist_id)->with('star')->get();
-        $stars = 0;
+        $psychologist = $this->psychologist_repository->find($psychologist_id);
 
-        foreach ($queries as $query){
-            if($query->star)
-                $stars += $query->star->star;
-        }
-
-        $qtd_queries = $queries->count();
-        $number_of_possible_stars = $qtd_queries * 5;
-
-        if(!$number_of_possible_stars)
-            return 5;
-
-        $percentage_star = $stars / $number_of_possible_stars;
-        $qtd_stars = ceil(($percentage_star * 5));
-
-        return $qtd_stars;
+        return $psychologist->stars();
     }
 }
