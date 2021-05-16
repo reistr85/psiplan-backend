930c8a9cd3b719d2f90c7c3063c7ed465051e6de
Renan Reis <reistr85@gmail.com>
2021-05-15 22:12:12 -0300

    Foi adicionado o link do boleto nos pagamentos

--- app/Repositories/PagarmeTransactionRepository.php
+++ app/Repositories/PagarmeTransactionRepository.php
@@ -29,7 +29,8 @@ class PagarmeTransactionRepository extends BaseRepository
     {
         return $this->model::select("pagarme_transactions.id", "pagarme_transactions.status",
             "pagarme_transactions.status as status_payment", "pagarme_transactions.amount",
-            "pagarme_transactions.transaction_id", "pagarme_transactions.created_at", "queries.day_hour",
+            "pagarme_transactions.transaction_id", "pagarme_transactions.payment_method", "pagarme_transactions.billet_url",
+            "pagarme_transactions.created_at", "queries.day_hour",
             "queries.id as query_id")
             ->where('client_id', $client_id)
             ->join('queries', 'query_id', 'queries.id')
